3836e98035270deb3a6696f55e260b75ac7ffd0c
Hidalgo71 <y.ozdemir71@gmail.com>
2021-11-10 15:22:06 +0300

    Updated.

--- src/main/java/mainAmazon.java
+++ src/main/java/mainAmazon.java
@@ -7,13 +7,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class mainAmazon
 {
-    public static void main(String[] args)
+    WebDriver amzDriver = new ChromeDriver();
+
+    public void main(String[] args)
     {
         DesiredCapabilities chDCap = DesiredCapabilities.chrome();
         chDCap.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
         chDCap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);                    //Certification
         System.setProperty("webdriver.chrome.driver", "D:\\Docs\\Drivers\\chromedriver.exe");
-        WebDriver amzDriver = new ChromeDriver();
+
         amzDriver.get("https://www.amazon.com.tr");
         amzDriver.manage().window().maximize();
         amzDriver.findElement(By.id("sp-cc-accept")).click();
@@ -31,14 +33,11 @@ public class mainAmazon
             amzDriver.findElement(By.id("twotabsearchtextbox")).clear();
 
         }
-
-
     }
 
-    private static void SearchItem()
+    private void SearchItem()
     {
-        WebDriver srcDriver = new ChromeDriver();
-        System.out.println(srcDriver.getTitle());
+
     }
 
 }
